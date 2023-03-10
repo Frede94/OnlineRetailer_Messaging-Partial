537e1284288ac69b7dc762e3b13339892d6b5dd6
Frede94 <32297979+Frede94@users.noreply.github.com>
2023-03-10 11:57:06 +0100

    Update CustomerRepository.cs

--- CustomerApi/Data/CustomerRepository.cs
+++ CustomerApi/Data/CustomerRepository.cs
@@ -8,25 +8,30 @@ namespace CustomerApi.Data
         private readonly CustomerApiContext db;
         public CustomerRepository(CustomerApiContext contex) { db = contex; }
 
-        public Customer IRepository<Customer>.Add(Customer entity)
+        public IEnumerable<Customer> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        Customer IRepository<Customer>.Add(Customer entity)
         {
             var newCustomer = db.Customers.Add(entity).Entity;
             db.SaveChanges();
             return newCustomer;
         }
 
-        public void IRepository<Customer>.Edit(Customer entity)
+        void IRepository<Customer>.Edit(Customer entity)
         {
             db.Entry(entity).State = EntityState.Modified;
             db.SaveChanges();
         }
 
-        public Customer IRepository<Customer>.Get(int customerId)
+         Customer IRepository<Customer>.Get(int customerId)
         {
             return db.Customers.FirstOrDefault(c => c.CustomerId == customerId);
         }
 
-        public void IRepository<Customer>.Remove(int customerId)
+        void IRepository<Customer>.Remove(int customerId)
         {
             var customer = db.Customers.FirstOrDefault(c => c.CustomerId == customerId);
             db.Customers.Remove(customer);
