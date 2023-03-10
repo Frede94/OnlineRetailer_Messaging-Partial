006bb8f6bc90273a899f8125ec3fe5df2e554ab3
Frede94 <32297979+Frede94@users.noreply.github.com>
2023-03-10 13:52:28 +0100

    Update MessageListener.cs

--- CustomerApi/Infrastructure/MessageListener.cs
+++ CustomerApi/Infrastructure/MessageListener.cs
@@ -1,6 +1,24 @@
-﻿namespace CustomerApi.Infrastructure;
+﻿using System;
+using System.Threading;
+using EasyNetQ;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CustomerApi.Infrastructure;
 
 public class MessageListener
 {
-    
+
+
+    public async Task Handle(OrderCreatedEvent @event)
+    {
+        var customer = _repository.Get(@event.CustomerId);
+        var alldasd = _repository.GetAll();
+
+        if (customer != null)
+        {
+            //TODO: check the customer’s credit standing
+            //TODO: check if the customer has outstanding bills
+
+        }
+    }
 }
