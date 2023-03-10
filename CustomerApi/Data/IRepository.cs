1cb111b86668f80b341d64a969bc0641ebd46f25
Frede94 <32297979+Frede94@users.noreply.github.com>
2023-03-10 11:20:20 +0100

    Update IRepository.cs

--- CustomerApi/Data/IRepository.cs
+++ CustomerApi/Data/IRepository.cs
@@ -3,9 +3,9 @@
     public interface IRepository<T>
     {
         IEnumerable<T> GetAll();
-        T GetValue(int id);
+        T Get(int id);
         T Add(T entity);
-        void Update(T entity);
-        void Delete(int id);   
+        void Edit(T entity);
+        void Remove(int id);   
     }
 }
