6cfa89d45554f3bec8a4e741e90fd4de478c3d50
WackyGlow <70566908+WackyGlow@users.noreply.github.com>
2023-03-10 10:55:32 +0100

    data

--- CustomerApi/Data/DbInitializer.cs
+++ CustomerApi/Data/DbInitializer.cs
@@ -1,6 +1,29 @@
-﻿namespace CustomerApi.Data
+﻿using CustomerApi.Models;
+
+namespace CustomerApi.Data
 {
-    public class DbInitializer
+    public class DbInitializer : IDbInitializer
     {
+        public void Initialize(CustomerApiContext context)
+        {
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+        
+            // Look for any Orders
+            if (context.Customers.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            List<Customer> orders = new List<Customer>
+            {
+                new Customer {
+                    
+                }
+            };
+
+            context.Customers.AddRange(orders);
+            context.SaveChanges();
+        }
     }
 }
