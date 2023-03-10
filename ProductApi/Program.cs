08f4c05ad9dd9d39d2c138124f0eaf76b2772ee0
Frede94 <32297979+Frede94@users.noreply.github.com>
2023-03-10 09:47:05 +0100

    Update Program.cs

--- ProductApi/Program.cs
+++ ProductApi/Program.cs
@@ -7,7 +7,7 @@ using SharedModels;
 var builder = WebApplication.CreateBuilder(args);
 
 // RabbitMQ connection string (I use CloudAMQP as a RabbitMQ server).
-// Remember to replace this connectionstring with your own.
+// Remember to replace this connectionstring with your own. 
 string cloudAMQPConnectionString =
     "host=hare.rmq.cloudamqp.com;virtualHost=npaprqop;username=npaprqop;password=type your password here";
 
